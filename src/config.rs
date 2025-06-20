15649a7edc37d9da94d5c6edaaf6043eafc03de2
chimu0126 <chimu0126@gmail.com>
2025-06-20 12:42:17 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rus.wlt0126.xyz"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rus.910808.xyz"];
 pub const RS_PUB_KEY: &str = "dIywnCS0SaMgWayUiVRtzsO0si55zqkSBMDCVw1VRkw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
