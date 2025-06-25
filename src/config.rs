603eaa881b658455f973c08259e9e9f7f566ced7
bingheyu <bingheyu@163.com>
2025-06-25 16:24:09 +0800

    改

--- src/config.rs
+++ src/config.rs
@@ -100,13 +100,13 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.1984123.xyz"];
+pub const RS_PUB_KEY: &str = "RViK5bM+Ua8geLWlcSCumDjP02Cwbu6EqQEr3r8IWxQ=";
 
-pub const RENDEZVOUS_PORT: i32 = 21116;
-pub const RELAY_PORT: i32 = 21117;
-pub const WS_RENDEZVOUS_PORT: i32 = 21118;
-pub const WS_RELAY_PORT: i32 = 21119;
+pub const RENDEZVOUS_PORT: i32 = 52116;
+pub const RELAY_PORT: i32 = 52117;
+pub const WS_RENDEZVOUS_PORT: i32 = 52118;
+pub const WS_RELAY_PORT: i32 = 52119;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
