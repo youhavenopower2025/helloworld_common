03ebed4c3b820411634d96f576f78a30f866e9d1
wjw0518 <104183476+wjw0518@users.noreply.github.com>
2025-06-26 14:45:16 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["ru.wjw108.store"];
+pub const RS_PUB_KEY: &str = "CNk0m5F58xBONdgRx7Hhybzd8NXPnwpl0yPrMuu5Nwo="; 
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
