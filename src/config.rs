3a5e8c08c047fae0ee7365d3cc2473bf2bf4d128
peter-chen61 <afhtc0114@gmail.com>
2025-06-29 07:42:34 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["ruds.cckf.com"];
+pub const RS_PUB_KEY: &str = "92VIDpGaJ6rbZEH04YRVY2NhRKObwc2eOSxVz0KblcQ=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
