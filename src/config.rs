6564025263524129941fa873dea4548d43469e81
Jlynx <songzone@vip.qq.com>
2025-06-29 18:33:31 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,13 +100,13 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["longquanguzhen.com"];
+pub const RS_PUB_KEY: &str = "N9ePeXAtJvg3rlvQytNhLqy6mYqwpwhTXJ6fZoCzs1w=";
 
-pub const RENDEZVOUS_PORT: i32 = 21116;
-pub const RELAY_PORT: i32 = 21117;
-pub const WS_RENDEZVOUS_PORT: i32 = 21118;
-pub const WS_RELAY_PORT: i32 = 21119;
+pub const RENDEZVOUS_PORT: i32 = 22226;
+pub const RELAY_PORT: i32 = 22227;
+pub const WS_RENDEZVOUS_PORT: i32 = 22228;
+pub const WS_RELAY_PORT: i32 = 22229;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
