9bfa86ee8b2e0d888a9391b41a79806dad3edd4d
freehead88 <430936@163.com>
2025-07-05 20:27:16 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["xqdwj.whlydn.top"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.116.68.71"];
+pub const RS_PUB_KEY: &str = "tZiKNWbrrUzjH0dTZBnVMC7roRxZfnNO1kbTEtZh9LI=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
