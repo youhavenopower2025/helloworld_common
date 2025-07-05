23e64218462feb97c1c8892b310608f63d79c844
zlf7512 <47875000+zlf7512@users.noreply.github.com>
2025-07-05 14:04:07 +0700

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["thx.thsen.top"];
+pub const RS_PUB_KEY: &str = "MrTYg26AiiP+5yv0nDMvEqrJob9UqD7s7niuykGsmVE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
