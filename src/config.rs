ffe09e1f90837a35898b9b16e10595124b507112
freehead88 <430936@163.com>
2025-07-05 18:28:48 +0800

    修改默认的服务器和 key

--- src/config.rs
+++ src/config.rs
@@ -100,9 +100,9 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
-
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.116.68.71"];
+pub const RS_PUB_KEY: &str = "tZiKNWbrrUzjH0dTZBnVMC7roRxZfnNO1kbTEtZh9LI=";
+pub const RELAY_SERVERS: &[&str] = &["47.116.68.71"];
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
 pub const WS_RENDEZVOUS_PORT: i32 = 21118;
