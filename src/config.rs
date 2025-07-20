cc2b06fad8a17aa477555a42a0cf5a54d37b7741
yellowcaicai <41869689+yellowcaicai@users.noreply.github.com>
2025-07-20 21:23:46 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["10.0.0.1"];
+pub const RS_PUB_KEY: &str = "dIUBjOyQLrcUCyIpPLgnS3axyRc6gY66fG4KRWKZi+k=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
