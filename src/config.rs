ec7594e17f14f3b1d545dd68313f43d1b4c2d42d
tuchuang888 <tuchuang88@gmail.com>
2025-08-14 10:03:54 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["i.ccynas.cn"];
+pub const RS_PUB_KEY: &str = "5sVv+YkmXGvAZKdeIQZtZoLDlXM8H7z5qv5tu6ZFhk8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
