6b6c3d7aa3a39cbc688ba64553d875f478d63dfa
341183 <112993941+341183@users.noreply.github.com>
2025-08-15 17:46:29 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["10.22.2.57"];
+pub const RS_PUB_KEY: &str = "bVf8e2ZJkZAVkPvnBvzs3E74VTpwPBP6gcmDtx8mF0Q=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
