c2b716dcd70f20f8f7fbe3d12d55342304ab617f
unknown <nima.gooler@gmail.com>
2025-08-19 10:33:43 +0330

    add correct sepehrdesk

--- src/config.rs
+++ src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.sepehrdesk.irr"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.sepehrdesk.ir"];
 pub const RS_PUB_KEY: &str = "T19NXwgKjXSwFfi1HzPv3cZh982QQPXAn92JOzy72TI=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
