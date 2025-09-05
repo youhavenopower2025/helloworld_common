efb6edcf73d799a753c17b7da74741cd6981f8eb
tptoday <chenbintx@126.com>
2025-09-05 13:40:33 +0800

    config

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["39.162.50.119"];
+pub const RS_PUB_KEY: &str = "5vAtxY2kzzZ2qCTLpR6sGahUvIX7VagRvOxVgnpfCwQ=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
