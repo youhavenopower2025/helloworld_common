346f20d4ab870c2104443436c8747b3b29abe471
550W-hub <jkchen.chn@gmail.com>
2025-09-15 12:14:03 +0800

    改为自己的 ID/ 中继服务器

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["120.26.247.48"];
+pub const RS_PUB_KEY: &str = "wdwxYnKfwe5zyF029Lam8bc0wiM1iWFZPy8KNhg3bGU=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
