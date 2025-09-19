e25b72a128320851f401867aeec9bb7e260126b1
youhavenopower2025 <youhavenopower2025@outlook.com>
2025-09-19 17:42:27 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,13 +100,13 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["156.248.78.176"];
+pub const RS_PUB_KEY: &str = "esxQqmFYXXVy3mphoSkTCzn3JfvzJW98WngnirHZ1Ww=";
 
-pub const RENDEZVOUS_PORT: i32 = 21116;
-pub const RELAY_PORT: i32 = 21117;
-pub const WS_RENDEZVOUS_PORT: i32 = 21118;
-pub const WS_RELAY_PORT: i32 = 21119;
+pub const RENDEZVOUS_PORT: i32 = 52226;
+pub const RELAY_PORT: i32 = 52227;
+pub const WS_RENDEZVOUS_PORT: i32 = 52228;
+pub const WS_RELAY_PORT: i32 = 52229;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
